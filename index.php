a130f46c747c73372b009e42e0401d903b2ae1a4
JosiahZambony <animation402@gmail.com>
2022-03-27 22:30:57 +0800

    Adding a skeleton layout of the project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>index.php</title>
+    </head>
+    <body>
+        <?php
+            echo phpinfo();
+        ?>
+    </body>
+</html>
