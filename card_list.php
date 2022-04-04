8c2e2a86494f7ea81c3248d4a8078b5f65898be0
18014238 <69470946+18014238@users.noreply.github.com>
2022-04-04 15:29:18 +0800

    Added changes

--- card_list.php
+++ card_list.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <!-- Required meta tags -->
         <meta charset="UTF-8">
@@ -9,13 +10,13 @@
         <meta name="author" content="Kar Hoe">
         <meta name="author" content="Tham Josiah">
         <meta name="author" content="Xiang Xi">
-        
+
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet"
               href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
               integrity=
-                "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
               crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
         <script defer
@@ -28,17 +29,65 @@
                 integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
                 crossorigin="anonymous">
         </script>
-        
+
         <title>Card List</title>
     </head>
     <body>
+
         <?php
-            include "nav_bar.php";
+        include "nav_bar.php";
         ?>
         <section class="row-md">
             <div class="container p-3">
-                <h1>Available Cards</h1>
+                <h1 class="display-4">Available Cards</h1>
             </div>
         </section>
-    </body>
+        <section class="cards">
+             <?php
+            $imagesDirectory = "card/";
+
+            if (is_dir($imagesDirectory)) {
+                $opendirectory = opendir($imagesDirectory);
+
+                while (($image = readdir($opendirectory)) !== false) {
+                    if (($image == '.') || ($image == '..')) {
+                        continue;
+                    }
+
+                    $imgFileType = pathinfo($image, PATHINFO_EXTENSION);
+                    
+                    $imgFileName = pathinfo($image, PATHINFO_FILENAME);
+
+                    if (($imgFileType == 'jpg') || ($imgFileType == 'png')) {
+                        ?>     
+            <div class ="card">
+                <div class ="card-img">    
+                    <?php
+                    echo "<img src='card/" . $image . "'>";
+                    ?>" 
+                </div>
+                <div class ='card-info'>
+                    <h1><?php echo $imgFileName?></h1>
+                    <p class="price">Price</p>
+                    <p>Description</p>
+                    <p><button>Add to Cart</button></p>
+
+              </div>
+
+            </div>
+
+<?php
+
+}
+}
+
+closedir($opendirectory);
+}
+?>
+  
+
+
+        </section>
+</html>
+</body>
 </html>
