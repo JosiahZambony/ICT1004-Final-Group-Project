55f568bd2bb68e92300d96ea33478303c62ffe7b
tandejun <dejun.18@ichat.sp.edu.sg>
2022-04-08 18:58:18 +0800

    check desc
    added latest cards
    cleaned up code
    fixed admin session bug

--- edit_cart_process.php
+++ edit_cart_process.php
@@ -33,13 +33,7 @@
     </head>
     <body>
         <?php
-        function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
 
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-        }
         include "nav_bar.php";
         ?>
         <section>
